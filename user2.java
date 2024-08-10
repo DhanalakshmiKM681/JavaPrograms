70fd20e01e6f44c52b60129d1cad5ea48b54c834
DhanalakshmiKM681 <dhanalakshmikm29@gmail.com>
2024-08-10 16:13:59 +0530

    Add files via upload

---
+++ user2.java
@@ -0,0 +1,14 @@
+class user2 {
+    int age;
+    String name;
+    String gender;
+
+    public void walk() {
+        //you do walking
+        System.out.println("walking");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello user2");
+    }
+}
